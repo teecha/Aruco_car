7e76cdc62fd5932a6c6379de2267550b3627382d
Teja Madduri <teecha23@gmail.com>
2022-11-15 12:27:13 -0700

    added debug points

--- src/Marker_detection/aruco_video.py
+++ src/Marker_detection/aruco_video.py
@@ -87,7 +87,7 @@ while True:
 			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
 			topLeft = (int(topLeft[0]), int(topLeft[1]))
 			print("TopRight {} BottomRight {} BottomLeft {} TopLeft {}".format(topRight,bottomRight,bottomLeft,topLeft))
-			
+
 			# draw the bounding box of the ArUCo detection
 			cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
 			cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
