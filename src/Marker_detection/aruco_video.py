99aeb9378b8a626963a22d25f31579931b1c38a9
Teja Madduri <teecha23@gmail.com>
2022-11-18 00:29:26 -0700

    reformitting the code

--- src/Marker_detection/aruco_video.py
+++ src/Marker_detection/aruco_video.py
@@ -89,9 +89,9 @@ while True:
 			print("TopRight {} BottomRight {} BottomLeft {} TopLeft {}".format(topRight,bottomRight,bottomLeft,topLeft))
 
 			#detecting the pose of the markers
-			rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners, 0.02)
-			(rvec - tvec).any()
-			cv2.aruco.drawAxis(frame, rvec, tvec, 0.01)
+			#rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners, 0.02)
+			#(rvec - tvec).any()
+			#cv2.aruco.drawAxis(frame, rvec, tvec, 0.01)
 
 			# draw the bounding box of the ArUCo detection
 			cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
